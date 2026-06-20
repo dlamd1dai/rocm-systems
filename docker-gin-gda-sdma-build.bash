d4127e269a7e22e4b836a36c924b5f8fa172e456
Donglai Dai <donglai.dai@amd.com>
2026-06-20 17:21:27 -0400

    Update dockerfiles, build and test scripts.
    	renamed:    Dockerfile-rccl-gin-gda -> Dockerfile-rccl-gin-gda-sdma
    	renamed:    Dockerfile-rccl-gin-gda-ruby -> Dockerfile-rccl-gin-gda-sdma-ruby
    	renamed:    docker-gin-gda-build.bash -> docker-gin-gda-sdma-build.bash
    	renamed:    docker-gin-gda-ruby-build.bash -> docker-gin-gda-sdma-ruby-build.bash
    	renamed:    docker-gin-gda-ruby-test.bash -> docker-gin-gda-sdma-ruby-test.bash
    	renamed:    docker-gin-gda-test.bash -> docker-gin-gda-sdma-test.bash
    
    	modified:   docker-gin-gda-sdma-build.bash
    	modified:   docker-gin-gda-sdma-ruby-build.bash
    	modified:   docker-gin-gda-sdma-ruby-test.bash
    	modified:   docker-gin-gda-sdma-test.bash

--- docker-gin-gda-sdma-build.bash
+++ docker-gin-gda-sdma-build.bash
@@ -20,12 +20,14 @@ RCCL_IMAGE_REQUIRE_MLX5_DMABUF_SYMBOLS="${RCCL_IMAGE_REQUIRE_MLX5_DMABUF_SYMBOLS
 # Dockerfile COPY extra-rdma-debs/ requires the directory in build context (optional .deb install).
 mkdir -p extra-rdma-debs
 
+N=1
 ${DOCKER_CMD} build -f ${DOCKERFILE} -t ${DOCKER_IMAGE} \
     --no-cache \
     --build-arg GPU_TARGETS=${TARGET_GPU_ARCH} \
     --build-arg USE_LOCAL_SRC=${USE_LOCAL_SRC} \
     --build-arg ROCSHMEM_USE_SDMA=${ROCSHMEM_USE_SDMA} \
     --build-arg RCCL_IMAGE_REQUIRE_MLX5_DMABUF_SYMBOLS=${RCCL_IMAGE_REQUIRE_MLX5_DMABUF_SYMBOLS} \
+    --build-arg ROCSHMEM_CACHE_BUST=$((N++)) \
     .
 ${DOCKER_CMD} image inspect "${DOCKER_IMAGE}" >/dev/null
 
