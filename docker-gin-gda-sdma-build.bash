1370b35af1864f6ea41e44c6404898105b4023da
Donglai Dai <donglai.dai@amd.com>
2026-06-21 02:50:51 -0500

    Change the build scripts to ensure that SDMA/Anvil lib
    is built and included in the rocshmem lib in the docker image.

--- docker-gin-gda-sdma-build.bash
+++ docker-gin-gda-sdma-build.bash
@@ -2,20 +2,20 @@
 
 # Test#5 (GIN Anvil-SDMA, NCCL_GIN_TYPE=6) needs rocSHMEM built with USE_SDMA=ON. Default
 # ROCSHMEM_USE_SDMA=1 passes --build-arg to the Dockerfile; set ROCSHMEM_USE_SDMA=0 to opt out.
+# ROCSHMEM_USE_SDMA="${ROCSHMEM_USE_SDMA:-1}"
 
 DOCKER_CMD=docker
 DOCKERFILE="Dockerfile-rccl-gin-gda-sdma"
 DOCKER_IMAGE="rccl-gin-gda-sdma-713"
 TARGET_GPU_ARCH=gfx950
 USE_LOCAL_SRC=1
-ROCSHMEM_USE_SDMA="${ROCSHMEM_USE_SDMA:-1}"
+ROCSHMEM_USE_SDMA=1
 
-N=1
 ${DOCKER_CMD} build -f ${DOCKERFILE} -t ${DOCKER_IMAGE} \
     --no-cache \
     --build-arg GPU_TARGETS=${TARGET_GPU_ARCH} \
     --build-arg USE_LOCAL_SRC=${USE_LOCAL_SRC} \
     --build-arg ROCSHMEM_USE_SDMA=${ROCSHMEM_USE_SDMA} \
-    --build-arg ROCSHMEM_CACHE_BUST=$((N++)) .
+    .
 ${DOCKER_CMD} image inspect "${DOCKER_IMAGE}" >/dev/null
 
