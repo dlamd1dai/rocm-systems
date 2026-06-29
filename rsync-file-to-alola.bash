8a9c1dc7e87822cc39933ef5ac9d2ec1acf37587
Donglai Dai <donglai.dai@amd.com>
2026-06-28 22:08:25 -0500

    Add another compute node as default hostname for destination
    	modified:   rsync-file-to-alola.bash

--- rsync-file-to-alola.bash
+++ rsync-file-to-alola.bash
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
 
 MYFILE=${1:-"./ddai-gin-perf.log"}
-IP_ADDR=${2:-"ctr2-alola-login-01.adc.amd.com"}
+# IP_ADDR=${2:-"ctr2-alola-login-01.adc.amd.com"}
+IP_ADDR=${2:-"ctr-cx63-mi300x-12.adc.amd.com"}
 
 rsync -avzL $MYFILE dondai@${IP_ADDR}:/home/AMD/dondai/rocm-systems.git/
 
