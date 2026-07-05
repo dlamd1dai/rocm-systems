ed0e398f5764f1329026e1aaeb36029ddbd63451
Donglai Dai <donglai.dai@amd.com>
2026-07-04 21:26:36 -0500

    Update the rsync script due to host node change.
    	modified:   rsync-file-to-alola.bash

--- rsync-file-to-alola.bash
+++ rsync-file-to-alola.bash
@@ -2,7 +2,8 @@
 
 MYFILE=${1:-"./ddai-gin-perf.log"}
 # IP_ADDR=${2:-"ctr2-alola-login-01.adc.amd.com"}
-IP_ADDR=${2:-"ctr-cx63-mi300x-12.adc.amd.com"}
+# IP_ADDR=${2:-"ctr-cx63-mi300x-12.adc.amd.com"}
+IP_ADDR=${2:-"ctr-cx64-mi300x-4.adc.amd.com"}
 
 rsync -avzL $MYFILE dondai@${IP_ADDR}:/home/AMD/dondai/rocm-systems.git/
 
