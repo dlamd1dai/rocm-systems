045da460debd9c7574c1c8ed6d07df3e689964db
Donglai Dai <donglai.dai@amd.com>
2026-07-07 11:44:02 -0500

    Update the rsync script for this project.
    	modified:   rsync-file-to-alola.bash

--- rsync-file-to-alola.bash
+++ rsync-file-to-alola.bash
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 
 MYFILE=${1:-"./ddai-gin-perf.log"}
-# IP_ADDR=${2:-"ctr2-alola-login-01.adc.amd.com"}
+IP_ADDR=${2:-"ctr2-alola-login-01.adc.amd.com"}
 # IP_ADDR=${2:-"ctr-cx63-mi300x-12.adc.amd.com"}
-IP_ADDR=${2:-"ctr-cx64-mi300x-4.adc.amd.com"}
+# IP_ADDR=${2:-"ctr-cx64-mi300x-4.adc.amd.com"}
 
 rsync -avzL $MYFILE dondai@${IP_ADDR}:/home/AMD/dondai/rocm-systems.git/
 
